0d091fa8d35d63a19613f7554354c9e779e9b1b5
Milan <kmilanm13@gmail.com>
2024-03-16 15:57:32 +0000

    [Milan] Laid-out the design for the Discovery posts

--- app/src/main/java/ie/thirdfloor/csis/ul/laedsgo/ui/posts/placeholder/PlaceholderContent.java
+++ app/src/main/java/ie/thirdfloor/csis/ul/laedsgo/ui/posts/placeholder/PlaceholderContent.java
@@ -38,7 +38,7 @@ public class PlaceholderContent {
     }
 
     private static PlaceholderItem createPlaceholderItem(int position) {
-        return new PlaceholderItem(String.valueOf(position), "Item " + position, makeDetails(position));
+        return new PlaceholderItem(String.valueOf(position), "Milan Kovacs " + position, makeDetails(position));
     }
 
     private static String makeDetails(int position) {
