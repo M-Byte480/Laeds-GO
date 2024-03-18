368b1928091f48884c0b393d56a9763d4b4bba73
Milan <kmilanm13@gmail.com>
2024-03-18 12:46:42 +0000

    [Milan] Disabled orientation, added event listeners to the buttons

--- app/src/main/java/ie/thirdfloor/csis/ul/laedsgo/MainActivity.java
+++ app/src/main/java/ie/thirdfloor/csis/ul/laedsgo/MainActivity.java
@@ -1,6 +1,8 @@
 package ie.thirdfloor.csis.ul.laedsgo;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -15,6 +17,7 @@ import ie.thirdfloor.csis.ul.laedsgo.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,4 +40,20 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
+    public void onLikeButtonClick(View view) {
+        Log.i(TAG, "Like Button Clicked");
+    }
+
+    public void onDislikeButtonClick(View view){
+        Log.i(TAG, "Dislike Button Clicked");
+    }
+
+    public void openComments(View view){
+        Log.i(TAG, "Opening Comments");
+    }
+
+    public void openProfile(View view){
+        Log.i(TAG, "Requesting to view profile");
+    }
 }
---
+++ app/src/main/java/ie/thirdfloor/csis/ul/laedsgo/eventhandlers/ClickHandler.java
@@ -0,0 +1,10 @@
+package ie.thirdfloor.csis.ul.laedsgo.eventhandlers;
+
+import android.util.Log;
+import android.view.View;
+
+public class ClickHandler {
+    private static final String TAG = "ClickHandler";
+    public void onLikeButtonClicked() {
+    }
+}
