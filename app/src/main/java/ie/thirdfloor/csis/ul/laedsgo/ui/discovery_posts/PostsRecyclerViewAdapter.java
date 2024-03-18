e59e374198ed4375bfe069b51650e5387b0c7137
Milan <kmilanm13@gmail.com>
2024-03-18 13:48:02 +0000

    [Milan] Visual implementation of like/dislike

--- app/src/main/java/ie/thirdfloor/csis/ul/laedsgo/ui/discovery_posts/PostsRecyclerViewAdapter.java
+++ app/src/main/java/ie/thirdfloor/csis/ul/laedsgo/ui/discovery_posts/PostsRecyclerViewAdapter.java
@@ -10,6 +10,7 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import ie.thirdfloor.csis.ul.laedsgo.R;
 import ie.thirdfloor.csis.ul.laedsgo.databinding.PostItemBinding;
 import ie.thirdfloor.csis.ul.laedsgo.entities.DiscoveryPost;
 import ie.thirdfloor.csis.ul.laedsgo.ui.discovery_posts.placeholder.PlaceholderContent;
@@ -66,14 +67,17 @@ public class PostsRecyclerViewAdapter extends RecyclerView.Adapter<PostsRecycler
         public final TextView mUsername;
         public final TextView mLikes;
         public final TextView mDislikes;
-        public boolean mIsLiked;
-        public boolean mIsDisliked;
+        public boolean isLiked;
+        public boolean isDisliked;
         public final TextView mLocation;
         public final TextView mTime;
         public final TextView mCommentCount;
         public final TextView mDate;
         public DiscoveryPost mItem;
-
+        public final ImageButton likeBtn;
+        public final ImageButton dislikeBtn;
+        public final ImageButton commentBtn;
+        public final ImageView pfp;
 
         public ViewHolder(PostItemBinding binding) {
             super(binding.getRoot());
@@ -86,10 +90,10 @@ public class PostsRecyclerViewAdapter extends RecyclerView.Adapter<PostsRecycler
             mCommentCount = binding.comments;
             mDate = binding.date;
 
-            ImageButton likeBtn = binding.likeButton;
-            ImageButton dislikeBtn = binding.dislikeButton;
-            ImageButton commentBtn = binding.commentsButton;
-            ImageView pfp = binding.pfp;
+            likeBtn = binding.likeButton;
+            dislikeBtn = binding.dislikeButton;
+            commentBtn = binding.commentsButton;
+            pfp = binding.pfp;
 
             likeBtn.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -130,10 +134,46 @@ public class PostsRecyclerViewAdapter extends RecyclerView.Adapter<PostsRecycler
 
         public void onLikeButtonClick(View view) {
             Log.i(TAG, "Like Button Clicked");
+
+            this.isLiked = !isLiked;
+
+            if(isLiked && isDisliked){
+                isDisliked = false;
+                updateDislike();
+            }
+
+
+            updateLike();
         }
 
         public void onDislikeButtonClick(View view) {
             Log.i(TAG, "Dislike Button Clicked");
+
+            this.isDisliked = !isDisliked;
+
+            if(isLiked && isDisliked){
+                isLiked = false;
+                updateLike();
+            }
+
+
+            updateDislike();
+        }
+
+        public void updateLike(){
+            if(isLiked){
+                likeBtn.setBackgroundResource(R.drawable.colour_like);
+            }else{
+                likeBtn.setBackgroundResource(R.drawable.like);
+            }
+        }
+
+        public void updateDislike(){
+            if (isDisliked) {
+                dislikeBtn.setBackgroundResource(R.drawable.colour_dislike);
+            }else{
+                dislikeBtn.setBackgroundResource(R.drawable.dislike);
+            }
         }
 
         public void openComments(View view) {
@@ -148,7 +188,5 @@ public class PostsRecyclerViewAdapter extends RecyclerView.Adapter<PostsRecycler
         public String toString() {
             return super.toString() + " '" + mContentView.getText() + "'";
         }
-
-
     }
 }
