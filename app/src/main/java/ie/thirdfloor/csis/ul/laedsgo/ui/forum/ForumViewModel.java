1c1a724ce42226cd5f5b6cbc702460e6be8e6cdc
Simon <21323208@studentmail.ul.ie>
2024-03-20 15:18:01 +0000

    [SS] Navbar finally complete...

--- app/src/main/java/ie/thirdfloor/csis/ul/laedsgo/ui/forum/ForumViewModel.java
+++ app/src/main/java/ie/thirdfloor/csis/ul/laedsgo/ui/forum/ForumViewModel.java
@@ -1,16 +1,16 @@
-package ie.thirdfloor.csis.ul.laedsgo.ui.notifications;
+package ie.thirdfloor.csis.ul.laedsgo.ui.forum;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class NotificationsViewModel extends ViewModel {
+public class ForumViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public NotificationsViewModel() {
+    public ForumViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is notifications fragment");
+        mText.setValue("This is dashboard fragment");
     }
 
     public LiveData<String> getText() {
