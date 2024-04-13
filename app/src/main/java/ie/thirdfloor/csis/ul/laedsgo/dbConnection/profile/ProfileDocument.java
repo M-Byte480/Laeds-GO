08e1757c897b43758c48acf97d19ca6b00ca9703
Milan <kmilanm13@gmail.com>
2024-04-13 17:01:23 +0100

    [Milan] Added likes and dislikes collection to profile

--- app/src/main/java/ie/thirdfloor/csis/ul/laedsgo/dbConnection/profile/ProfileDocument.java
+++ app/src/main/java/ie/thirdfloor/csis/ul/laedsgo/dbConnection/profile/ProfileDocument.java
@@ -1,6 +1,10 @@
 package ie.thirdfloor.csis.ul.laedsgo.dbConnection.profile;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import ie.thirdfloor.csis.ul.laedsgo.dbConnection.interfeces.IDocument;
 
 public class ProfileDocument implements IDocument {
@@ -11,6 +15,8 @@ public class ProfileDocument implements IDocument {
     public String profilePhoto = "";
     public String bio = "";
     public Date timestamp;
+    public ArrayList<Integer> likedPosts = new ArrayList();
+    public ArrayList<Integer> dislikedPosts = new ArrayList();
 
     @Override
     public String toString() {
@@ -22,6 +28,8 @@ public class ProfileDocument implements IDocument {
                 ", profilePhoto='" + profilePhoto + '\'' +
                 ", bio='" + bio + '\'' +
                 ", timestamp=" + timestamp +
+                ", likes={" + likedPosts.stream().map(Object::toString).collect(Collectors.joining(", ")) + "}" +
+                ", dislikes={" + dislikedPosts.stream().map(Object::toString).collect(Collectors.joining(", ")) + "}" +
                 '}';
     }
 }
