ca6edd7b152c3541ae1694875bccd0502bd3bf1c
Milan <kmilanm13@gmail.com>
2024-04-14 03:00:17 +0100

    [Milan] Top Navbar Gap fix

--- app/src/main/java/ie/thirdfloor/csis/ul/laedsgo/MainActivity.java
+++ app/src/main/java/ie/thirdfloor/csis/ul/laedsgo/MainActivity.java
@@ -2,6 +2,7 @@ package ie.thirdfloor.csis.ul.laedsgo;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.MutableLiveData;
